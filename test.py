dbf1a3dfb32e4e636cb2e14574555581748c8470
huang <z0908565970@gmail.com>
2023-10-03 10:39:34 +0800

    New

---
+++ test.py
@@ -0,0 +1,30 @@
+import openai
+import gradio as gr
+
+#openai.api_key = ""
+#openai.api_key = ""
+openai.api_key = "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"
+
+messages = [
+	{"role": "system", "content": "Counting his current word count and warn if it is less that 500 words."},
+	{"role": "system", "content": "Using traditional chinese to warning."},
+	#{"role": "system", "content": "Giving some feedback on user content."},
+]
+
+def chatbot(input):
+	if input:
+		messages.append({"role": "user", "content": input})
+		chat = openai.ChatCompletion.create(
+			model="gpt-3.5-turbo", messages=messages
+		)
+
+		reply = chat.choices[0].message.content
+		messages.append({"role": "assistant", "content": reply})
+
+		return reply
+
+inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
+outputs = gr.outputs.Textbox(label="Reply")
+
+gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="報告檢核系統", description="請在此輸入要檢測的報告段落", theme="compact").launch(share=True)
+#compact
